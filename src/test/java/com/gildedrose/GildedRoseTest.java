107ba10a053de56a3478142bc40a52b0795536c1
Peter Onneby <ponneby@xebia.fr>
2018-06-08 14:44:53 +0200

    Init project

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,17 @@
+package com.gildedrose;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class GildedRoseTest {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].name).isEqualTo("fixme");
+    }
+
+}
